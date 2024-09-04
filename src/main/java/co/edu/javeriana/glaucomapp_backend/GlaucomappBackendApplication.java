1fbadfd2182873f258fd537579f844b5419a014a
ericksgv <sangaravi22@gmail.com>
2024-09-03 22:27:09 -0500

    Initialize Spring Boot project structure

---
+++ src/main/java/co/edu/javeriana/glaucomapp_backend/GlaucomappBackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.glaucomapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlaucomappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlaucomappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/glaucomapp_backend/GlaucomappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.glaucomapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlaucomappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
