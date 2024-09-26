fb4f3f0a727a3e741e676548f938ec483e189bf2
ericksgv <sangaravi22@gmail.com>
2024-09-25 23:27:46 -0500

    Add integration test for verifying API Key module configuration

---
+++ src/test/java/co/edu/javeriana/glaucomapp_backend/glaucomascreening/GlaucomaScreeningModuleIntegrationTest.java
@@ -0,0 +1,24 @@
+package co.edu.javeriana.glaucomapp_backend.glaucomascreening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.test.ApplicationModuleTest;
+
+/**
+ * Integration test for the Glaucoma Screening module.
+ * <p>
+ * This class is responsible for testing the integration of the Glaucoma Screening module.
+ * It uses Spring Modulith's testing framework to verify that the module functions correctly
+ * within the application context.
+ * </p>
+ */
+@ApplicationModuleTest
+public class GlaucomaScreeningModuleIntegrationTest {
+
+    /**
+     * Verifies the integrity of the Glaucoma Screening module.
+     */
+    @Test
+    void verifyModule() {
+
+    }
+}
