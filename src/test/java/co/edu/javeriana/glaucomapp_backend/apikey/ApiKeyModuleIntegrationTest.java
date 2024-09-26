4cbd4866b1fbe02d8f6eaf1be9874310d07ac2c7
ericksgv <sangaravi22@gmail.com>
2024-09-25 22:51:36 -0500

    Add integration test for verifying API Key module configuration

---
+++ src/test/java/co/edu/javeriana/glaucomapp_backend/apikey/ApiKeyModuleIntegrationTest.java
@@ -0,0 +1,22 @@
+package co.edu.javeriana.glaucomapp_backend.apikey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.test.ApplicationModuleTest;
+
+/**
+ * Integration test for the API Key module.
+ * <p>
+ * This test verifies the correct configuration and wiring of the API Key module within the application.
+ * </p>
+ */
+@ApplicationModuleTest
+public class ApiKeyModuleIntegrationTest {
+
+    /**
+     * Verifies that the API Key module is correctly configured and initialized.
+     */
+    @Test
+    void verifyModule() {
+
+    }
+}
