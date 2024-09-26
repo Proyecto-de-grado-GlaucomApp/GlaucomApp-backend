912cd859b2d3146f316122f3d2a266e4ae92d07b
parraluisa <parran_ll@javeriana.edu.co>
2024-09-25 22:20:01 -0500

    Add: User (ophthalmologist) entity

---
+++ src/main/java/co/edu/javeriana/glaucomapp_backend/auth/model/User.java
@@ -0,0 +1,26 @@
+package co.edu.javeriana.glaucomapp_backend.auth.model;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name="password", nullable = false)
+    private String password;
+}
