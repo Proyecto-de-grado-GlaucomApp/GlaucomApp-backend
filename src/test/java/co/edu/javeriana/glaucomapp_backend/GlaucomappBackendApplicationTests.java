509aced8ed8480a6382048da79c11e7874183565
ericksgv <sangaravi22@gmail.com>
2024-09-25 23:30:54 -0500

    update: Integrate Spring Modulith for application context tests

--- src/test/java/co/edu/javeriana/glaucomapp_backend/GlaucomappBackendApplicationTests.java
+++ src/test/java/co/edu/javeriana/glaucomapp_backend/GlaucomappBackendApplicationTests.java
@@ -1,13 +1,31 @@
 package co.edu.javeriana.glaucomapp_backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.test.ApplicationModuleTest;
 
-@SpringBootTest
+/**
+ * Integration tests for the Glaucomapp Backend application.
+ * <p>
+ * This class is responsible for verifying the application context and ensuring that all
+ * application modules are loaded correctly. It serves as a basic test to check that the
+ * application can start without any issues.
+ * </p>
+ */
+@ApplicationModuleTest
 class GlaucomappBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    /**
+     * Tests that the application context loads successfully.
+     * <p>
+     * This test verifies that all modules within the GlaucomappBackend application are
+     * configured correctly and can be instantiated without errors. It utilizes the
+     * Spring Modulith framework to ensure that the application structure adheres to
+     * the defined module boundaries.
+     * </p>
+     */
+    @Test
+    void contextLoads() {
+        ApplicationModules.of(GlaucomappBackendApplication.class).verify();
+    }
 }
