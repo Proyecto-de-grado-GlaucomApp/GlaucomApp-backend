821276ee822c5e135b670dcfc27d7f705a7cae57
ericksgv <sangaravi22@gmail.com>
2024-10-16 11:26:18 -0500

    Update GlaucomaScreening Controller

--- src/main/java/co/edu/javeriana/glaucomapp_backend/glaucomascreening/GlaucomaScreeningController.java
+++ src/main/java/co/edu/javeriana/glaucomapp_backend/glaucomascreening/GlaucomaScreeningController.java
@@ -1,71 +1,87 @@
 package co.edu.javeriana.glaucomapp_backend.glaucomascreening;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-/**
- * Controller for handling glaucoma screening requests.
- * <p>
- * This controller provides endpoints for processing images related
- * to glaucoma screening, both from mobile applications and third-party
- * services. It currently simulates image processing results.
- * </p>
- */
+import lombok.extern.slf4j.Slf4j;
+
+
 @RestController
+@Slf4j
 @RequestMapping("/glaucoma-screening")
 public class GlaucomaScreeningController {
 
-    /**
-     * Processes an image file uploaded from a mobile device.
-     *
-     * @param imageFile the image file to be processed
-     * @return a ResponseEntity containing the result of the image processing
-     */
-    @PostMapping(value = "/mobile", 
-                 consumes = MediaType.MULTIPART_FORM_DATA_VALUE, 
-                 produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ImageProcessingResultDTO> processMobileImage(
-            @RequestParam("file") MultipartFile imageFile) {
-        // TODO: Implement actual image processing logic here
+        @Autowired
+        private GlaucomaScreeningService glaucomaScreeningService;
 
-        // Simulated result of image processing
-        ImageProcessingResultDTO result = new ImageProcessingResultDTO(
-                "http://example.com/processed-image.jpg",  // URL of the processed image
-                "Imagen procesada correctamente.",          // Diagnostic message
-                85,                                         // Percentage likelihood of glaucoma
-                2                                           // Stage on the Damage of Disc Scale (DDLS)
-        );
+        private static final Logger logger = LoggerFactory.getLogger(GlaucomaScreeningService.class);
 
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
+        /**
+         * Processes an image file uploaded (Service test)
+         *
+         * @param imageFile the image file to be processed
+         * @return a ResponseEntity containing the result of the image processing
+         */
+        @PostMapping(value = "/test", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+        public ResponseEntity<ImageProcessingResultDTO> processTestImage(
+                        @RequestParam("file") MultipartFile imageFile) {
+                System.out.println("Se ha procesado el archivo para probar el servicio");
 
-    /**
-     * Processes an image file uploaded from a third-party service.
-     *
-     * @param imageFile the image file to be processed
-     * @return a ResponseEntity containing the result of the image processing
-     */
-    @PostMapping(value = "/third-party", 
-                 consumes = MediaType.MULTIPART_FORM_DATA_VALUE, 
-                 produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ImageProcessingResultDTO> processThirdPartyImage(
-            @RequestParam("file") MultipartFile imageFile) {
-        // TODO: Implement actual image processing logic here
+                // Simulated result of image processing
+                ImageProcessingResultDTO result = new ImageProcessingResultDTO(
+                                "http://example.com/processed-image.jpg", // URL of the processed image
+                                "Imagen procesada correctamente.", // Diagnostic message
+                                85, // Percentage likelihood of glaucoma
+                                2 // Stage on the Damage of Disc Scale (DDLS)
+                );
 
-        // Simulated result of image processing
-        ImageProcessingResultDTO result = new ImageProcessingResultDTO(
-                "http://example.com/processed-image-third-party.jpg",  // URL of the processed image
-                "Imagen procesada correctamente para terceros.",          // Diagnostic message
-                90,                                                       // Percentage likelihood of glaucoma
-                3                                                         // Stage on the Damage of Disc Scale (DDLS)
-        );
+                return new ResponseEntity<>(result, HttpStatus.OK);
+        }
 
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
+        @GetMapping("/path")
+        public ResponseEntity<String> getMethodName() {
+                return ResponseEntity.ok("Hello, world!");
+        }
+        
+
+        @PostMapping(value = "/upload-image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+        public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file) {
+                if (file == null || file.isEmpty()) {
+                        return ResponseEntity.badRequest().body("No file provided or file is empty."); // Error code 400
+                }
+                try {
+                        String processedResult = glaucomaScreeningService.sendImageToApi(file);
+                        logger.info("Response from the service: " + processedResult);
+
+                        ImageProcessingResultDTO result = glaucomaScreeningService.generateResult();
+
+                        return new ResponseEntity<>(result, HttpStatus.OK);
+
+                } catch (RuntimeException e) {
+                        if (e.getMessage().contains("Client error")) {
+                                logger.error("Client error: " + e.getMessage());
+                                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                                                .body("Request error: " + e.getMessage());
+                        } else if (e.getMessage().contains("Server error")) {
+                                logger.error("Server error: " + e.getMessage());
+                                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                                .body("External service error: " + e.getMessage());
+                        } else {
+                                logger.error("Unexpected error: " + e.getMessage());
+                                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                                .body("An unexpected error occurred.");
+                        }
+                }
+        }
+
 }
