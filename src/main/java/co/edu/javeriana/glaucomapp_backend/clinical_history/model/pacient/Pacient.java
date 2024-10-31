c490f91fa128ad538d9d03c14ef05eb3dc1b0033
parraluisa <parran_ll@javeriana.edu.co>
2024-10-31 17:20:01 -0500

    Enhance Pacient class: improve Javadoc comments for better clarity and documentation

--- src/main/java/co/edu/javeriana/glaucomapp_backend/clinical_history/model/pacient/Pacient.java
+++ src/main/java/co/edu/javeriana/glaucomapp_backend/clinical_history/model/pacient/Pacient.java
@@ -1,39 +1,40 @@
 /**
  * Represents a patient in the GlaucomApp system.
+ * Each patient is uniquely identified by a UUID and has a unique combination of 
+ * ophthalmologist ID and cedula (identification number).
  * 
- * This entity is mapped to the "pacient" table in the database and contains
- * information about a patient, including their ID, cedula, name, associated
- * ophthalmologist user, and a list of exams.
+ * This class is annotated with JPA annotations to map it to a database table named "pacient".
+ * It uses Lombok annotations to generate boilerplate code such as getters, setters, constructors, 
+ * and the builder pattern.
+ * 
+ * Attributes:
+ * 
+ *   id - The unique identifier for the patient, generated automatically.
+ *   cedula - The identification number of the patient, cannot be null.
+ *   name - The name of the patient, cannot be null.
+ *   doctorId - The UUID of the ophthalmologist associated with the patient, cannot be null.
+ *   exams - A list of exams associated with the patient, with cascade operations and orphan removal enabled.
+ * 
+ * 
+ * Constraints:
+ * 
+ *   Unique constraint on the combination of ophthalmologist ID and cedula.
+ * 
+ * 
+ * Overrides:
+ * 
+ *   toString - Provides a string representation of the patient object.
+ * 
  * 
  * Annotations:
  * 
- *   {@link Entity} - Specifies that this class is an entity and is mapped to a database table.
- *   {@link Table} - Specifies the primary table for the annotated entity and unique constraints.
- *   {@link Id} - Specifies the primary key of an entity.
- *   {@link GeneratedValue} - Provides for the specification of generation strategies for the values of primary keys.
- *   {@link Column} - Used to specify the mapped column for a persistent property or field.
- *   {@link ManyToOne} - Defines a single-valued association to another entity class that has many-to-one multiplicity.
- *   {@link JoinColumn} - Specifies a column for joining an entity association or element collection.
- *   {@link OneToMany} - Defines a one-to-many relationship between this entity and another entity.
- *   {@link CascadeType} - Defines the set of cascade operations that are propagated to the associated entity.
- *   {@link lombok.AllArgsConstructor} - Generates a constructor with 1 parameter for each field in the class.
- *   {@link lombok.Builder} - Produces complex builder APIs for your classes.
- *   {@link lombok.Data} - Generates getters for all fields, a useful toString method, and hashCode and equals implementations.
- *   {@link lombok.NoArgsConstructor} - Generates a no-args constructor.
- * 
- * 
- * Fields:
- * 
- *   {@code id} - The unique identifier for the patient, generated automatically.
- *   {@code cedula} - The cedula (identification number) of the patient, cannot be null.
- *   {@code name} - The name of the patient, cannot be null.
- *   {@code ophthalUser} - The ophthalmologist user associated with the patient, cannot be null.
- *   {@code Exams} - The list of exams associated with the patient, with cascade operations and orphan removal enabled.
- * 
- * 
- * Methods:
- * 
- *   {@code toString()} - Returns a string representation of the patient object.
+ *   @Entity - Specifies that this class is an entity and is mapped to a database table.
+ *   @Table - Specifies the details of the table that this entity is mapped to.
+ *   @Id - Specifies the primary key of the entity.
+ *   @GeneratedValue - Provides the specification of generation strategies for the primary keys.
+ *   @Column - Specifies the details of the column to which a field or property will be mapped.
+ *   @OneToMany - Specifies a one-to-many relationship with another entity.
+ *   @Data, @Builder, @AllArgsConstructor, @NoArgsConstructor - Lombok annotations to reduce boilerplate code.
  * 
  */
 package co.edu.javeriana.glaucomapp_backend.clinical_history.model.pacient;
@@ -41,15 +42,12 @@ package co.edu.javeriana.glaucomapp_backend.clinical_history.model.pacient;
 import java.util.List;
 import java.util.UUID;
 
-import co.edu.javeriana.glaucomapp_backend.auth.exposed.MyUser;
 import co.edu.javeriana.glaucomapp_backend.clinical_history.model.exam.Exam;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
