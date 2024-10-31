9a82e30874f4b0a6bd24136bfdc89718e41c6e2d
parraluisa <parran_ll@javeriana.edu.co>
2024-10-31 16:22:10 -0500

    Enhance AuthService: update Javadoc comments for improved clarity on authentication and account management methods

--- src/main/java/co/edu/javeriana/glaucomapp_backend/auth/service/AuthService.java
+++ src/main/java/co/edu/javeriana/glaucomapp_backend/auth/service/AuthService.java
@@ -1,11 +1,7 @@
+
 /**
- * AuthService interface provides methods for user authentication operations.
- * It includes methods for user registration, login, and logout.
- * 
- * Methods:
- * - {@link #register(MyUser)}: Registers a new user.
- * - {@link #login(LogInForm, HttpServletResponse)}: Logs in a user with the provided login form and sets the response.
- * - {@link #logout(HttpServletResponse)}: Logs out the current user and sets the response.
+ * AuthService interface provides methods for user authentication and account management.
+ * It includes methods for user registration, login, logout, token refresh, and account closure.
  */
 package co.edu.javeriana.glaucomapp_backend.auth.service;
 
