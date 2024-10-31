8c4e54549c71cfc2d4ae560b4654c11a28f9a78f
parraluisa <parran_ll@javeriana.edu.co>
2024-10-31 17:20:19 -0500

    Enhance PatientEventService: add Javadoc comments for improved clarity and documentation

--- src/main/java/co/edu/javeriana/glaucomapp_backend/clinical_history/service/impl/PatientEventService.java
+++ src/main/java/co/edu/javeriana/glaucomapp_backend/clinical_history/service/impl/PatientEventService.java
@@ -1,3 +1,24 @@
+/**
+ * Service class for handling patient-related events.
+ * This service listens for events related to patients and performs necessary actions.
+ * 
+ * Dependencies:
+ * - PacientRepository: Repository for accessing patient data.
+ * - ExamRepository: Repository for accessing exam data.
+ * - S3Service: Service for handling S3 operations.
+ * 
+ * Methods:
+ * - on(UUID ophtalmologistId): Listens for events with the given ophthalmologist ID, retrieves all patients
+ *   associated with the doctor, deletes their exams and patients from the repositories, and deletes the 
+ *   associated images from S3.
+ * 
+ * Annotations:
+ * - @Service: Indicates that this class is a service component in the Spring context.
+ * - @RequiredArgsConstructor: Generates a constructor with required arguments (final fields).
+ * - @ApplicationModuleListener: Marks the method to listen for application module events.
+ * 
+ * @param ophtalmologistId The ID of the ophthalmologist whose patients' data needs to be processed.
+ */
 package co.edu.javeriana.glaucomapp_backend.clinical_history.service.impl;
 
 import java.util.ArrayList;
@@ -13,7 +34,7 @@ import co.edu.javeriana.glaucomapp_backend.common.S3.S3Service;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.modulith.events.ApplicationModuleListener;
-import org.springframework.stereotype.Component;
+
 
 @Service
 @RequiredArgsConstructor
@@ -25,26 +46,19 @@ public class PatientEventService {
 
     @ApplicationModuleListener
     void on(UUID ophtalmologistId){
-                System.out.println("Doctor deleted event received");
 
         //initialize an empty list of url images
         List<String> urlImages = new ArrayList<>();
-
-        // Encuentra y elimina los pacientes asociados al oftalmólogo
+        //get all the patients of the doctor
         List<Pacient> patients = pacientRepository.findAllPacientsByDoctorId(ophtalmologistId);
-        System.out.println("Patients found: " + patients.size());
         if (!patients.isEmpty()) {
-            System.out.println("Into pacients condiiton");
             patients.forEach(pacient -> {
                 pacient.getExams().forEach(exam -> urlImages.add(exam.getUrlImage()));
                 examRepository.deleteAll(pacient.getExams());
                 pacientRepository.delete(pacient);
             });
-            System.out.println("urlImages: " + urlImages);
             urlImages.forEach(urlImage -> {
-                System.out.println("Deleting image: " + urlImage);
                 s3Service.deleteImage(urlImage);
-                //deleteImage(urlImage);
             });
         }
     }
