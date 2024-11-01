23926e00ab222877d74dc618d7e0002ce0a248d3
parraluisa <parran_ll@javeriana.edu.co>
2024-10-31 19:00:53 -0500

    Enhance Appointment class: improve Javadoc comments for better clarity and documentation

--- src/main/java/co/edu/javeriana/glaucomapp_backend/clinical_history/service/impl/ExamServiceImpl.java
+++ src/main/java/co/edu/javeriana/glaucomapp_backend/clinical_history/service/impl/ExamServiceImpl.java
@@ -1,35 +1,26 @@
 /**
  * Service implementation for managing exams.
- * This class provides methods to save, retrieve, and delete exams for patients.
- * It ensures that the operations are performed by authorized ophthalmologists.
+ * This service provides methods to save, retrieve, and delete exams for patients.
+ * It also verifies the ophthalmologist's access to the exams.
  * 
  * Dependencies:
- *
- *   {@link ExamRepository}
- *   {@link MyUserRepository}
- *   {@link PacientRepository}
- * 
+ * - ExamRepository: Repository for accessing exam data.
+ * - S3Service: Service for handling S3 operations.
+ * - ReviewOphthalmologistEvent: Event for verifying ophthalmologist access.
+ * - PacientRepository: Repository for accessing patient data.
  * 
  * Methods:
+ * - saveExam(String ophtalIdString, ExamRequest examRequest): Saves a new exam for a patient.
+ * - getExamsByPacient(String ophtalIdString, String pacientIdString, int startIndex, int endIndex): Retrieves a list of exams for a patient.
+ * - deleteExam(String ophtalIdString, String pacientIdString, String examIdString): Deletes an exam for a patient.
+ * - getExamById(String ophtalIdString, String pacientIdString, String examIdString): Retrieves an exam by its ID.
  * 
- *   {@link #saveExam(String, ExamRequest)} - Saves a new exam for a patient.
- *   {@link #getExamsByPacient(String, String, int, int)} - Retrieves a list of exams for a patient within a specified range.
- *   {@link #deleteExam(String, String, String)} - Deletes an exam for a patient.
- *   {@link #getExamById(String, String, String)} - Retrieves an exam by its ID.
- *   {@link #verifyExam(UUID, UUID, UUID)} - Verifies the relationship between an exam, patient, and ophthalmologist.
- * 
- * 
- * Annotations:
- * 
- *   {@link Service} - Indicates that this class is a service component in the Spring framework.
- *   {@link Transactional} - Ensures that the delete operation is performed within a transaction.
- * 
- * 
- * Exceptions:
- * 
- *   {@link IllegalArgumentException} - Thrown when invalid IDs are provided or when no exams are found.
- *   {@link AccessDeniedException} - Thrown when unauthorized access to an exam is attempted.
- * 
+ * Private Methods:
+ * - findPacientByCedulaAndDoctorId(String cedula, UUID ophtalId): Finds a patient by their cedula and doctor ID.
+ * - createExamFromRequest(ExamRequest examRequest, Pacient pacient): Creates an exam entity from an exam request.
+ * - mapToExamRes(Exam exam): Maps an exam entity to an ExamRes response.
+ * - mapToExamsResponse(Exam exam): Maps an exam entity to an ExamsResponse response.
+ * - verifyExam(UUID ophtalId, UUID pacientId, UUID examId): Verifies the access to an exam.
  */
 package co.edu.javeriana.glaucomapp_backend.clinical_history.service.impl;
 
@@ -41,6 +32,7 @@ import java.util.stream.Collectors;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
+
 import co.edu.javeriana.glaucomapp_backend.clinical_history.model.exam.Exam;
 import co.edu.javeriana.glaucomapp_backend.clinical_history.model.exam.ExamRequest;
 import co.edu.javeriana.glaucomapp_backend.clinical_history.model.exam.ExamRes;
@@ -49,8 +41,9 @@ import co.edu.javeriana.glaucomapp_backend.clinical_history.model.pacient.Pacien
 import co.edu.javeriana.glaucomapp_backend.clinical_history.repository.ExamRepository;
 import co.edu.javeriana.glaucomapp_backend.clinical_history.repository.PacientRepository;
 import co.edu.javeriana.glaucomapp_backend.clinical_history.service.ExamService;
+import co.edu.javeriana.glaucomapp_backend.clinical_history.service.ReviewOphthalmologistEvent;
 import co.edu.javeriana.glaucomapp_backend.common.S3.S3Service;
-import jakarta.persistence.EntityNotFoundException;
+
 import jakarta.transaction.Transactional;
 
 @Service
@@ -59,113 +52,72 @@ public class ExamServiceImpl implements ExamService {
     
     private final ExamRepository examRepository;
     private final S3Service s3Service;
+    private final ReviewOphthalmologistEvent reviewOphthalmologistEvent;
 
-    
-    //private final MyUserRepository myUserRepository;
-
-    
     private final PacientRepository pacientRepository;
 
     public ExamServiceImpl(ExamRepository examRepository,
-            PacientRepository pacientRepository, S3Service s3Service) {
+            PacientRepository pacientRepository, S3Service s3Service, ReviewOphthalmologistEvent reviewOphthalmologistEvent) {
         this.examRepository = examRepository;
         this.pacientRepository = pacientRepository;
         this.s3Service = s3Service;
+        this.reviewOphthalmologistEvent = reviewOphthalmologistEvent;
     }
 
     /**
      * Saves an exam for a given ophthalmologist and patient.
      *
-     * @param ophtalIdString the ID of the ophthalmologist as a string
+     * @param ophtalIdString the UUID string of the ophthalmologist
      * @param examRequest the request object containing exam details
-     * @throws IllegalArgumentException if the ophthalmologist ID is invalid or the patient is not found
+     * @throws IllegalArgumentException if the UUID string is invalid
      */
     @Override
     public void saveExam(String ophtalIdString, ExamRequest examRequest) {
-        System.out.println("ophtalIdString on SAVE EXAM: " + ophtalIdString);
         UUID ophtalId = UUID.fromString(ophtalIdString);
 
-        /*
-        // Verify that the ophtalmologist is correct
-        MyUser ophthalmologist = myUserRepository.findById(ophtalId)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid Ophthalmologist ID"));
- */
+        reviewOphthalmologistEvent.verifyOphtalmologist(ophtalIdString);
 
-        // Find ophtal pacient
-        System.out.println("we a re goig to get pacient by cedula and doctor Id: " + examRequest.cedula());
-        Pacient pacient = pacientRepository.findPacientByCedulaAndDoctorId(examRequest.cedula(), ophtalId);
-        System.out.println("PACIENT: " + pacient);
-        if (pacient == null) {
-            throw new IllegalArgumentException("Patient not found for the given cedula");
-        }
+        Pacient pacient = findPacientByCedulaAndDoctorId(examRequest.cedula(), ophtalId);
 
-        // Create new exam
-        Exam newExam = Exam.builder()
-                .name(examRequest.name())
-                .date(examRequest.date())
-                .urlImage(examRequest.urlImage())
-                .distanceRatio(examRequest.distanceRatio())
-                .perimeterRatio(examRequest.perimeterRatio())
-                .areaRatio(examRequest.areaRatio())
-                .neuroretinalRimPerimeter(examRequest.neuroretinalRimPerimeter())
-                .neuroretinalRimArea(examRequest.neuroretinalRimArea())
-                .excavationPerimeter(examRequest.excavationPerimeter())
-                .excavationArea(examRequest.excavationArea())
-                .state(examRequest.state())
-                .ddlStage(examRequest.ddlStage())
-                .pacient(pacient)
-                .build();
-
-        // Save exam
-        System.out.println("going to save nre exam: " + newExam);
+        Exam newExam = createExamFromRequest(examRequest, pacient);
         examRepository.save(newExam);
-        System.out.println("exam saved");
     }
 
     /**
      * Retrieves a list of exams for a specific patient within a specified range.
      *
-     * @param ophtalIdString the ID of the ophthalmologist as a string
-     * @param pacientIdString the ID of the patient as a string
-     * @param startIndex the starting index of the exams to return
-     * @param endIndex the ending index of the exams to return
-     * @return a list of ExamsResponse objects containing exam details
-     * @throws IllegalArgumentException if the ophthalmologist ID is invalid or no exams are found for the specified patient
+     * @param ophtalIdString the UUID string of the ophthalmologist
+     * @param pacientIdString the UUID string of the patient
+     * @param startIndex the starting index of the exams to retrieve
+     * @param endIndex the ending index of the exams to retrieve
+     * @return a list of ExamsResponse objects representing the exams for the patient
+     * @throws IllegalArgumentException if the UUID strings are invalid
      */
-    @Override
-    public List<ExamsResponse> getExamsByPacient(String ophtalIdString, String pacientIdString, int startIndex,
-            int endIndex) {
+    public List<ExamsResponse> getExamsByPacient(String ophtalIdString, String pacientIdString, int startIndex, int endIndex) {
         UUID ophtalId = UUID.fromString(ophtalIdString);
         UUID pacientId = UUID.fromString(pacientIdString);
+        reviewOphthalmologistEvent.verifyOphtalmologist(ophtalIdString);
 
-        /*
-        // Verify if the ophtal is valid
-        myUserRepository.findById(ophtalId)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid Ophthalmologist ID"));
-         */
-                // Obtain pacient exams
         List<Exam> exams = examRepository.findByPacient_DoctorIdAndPacient_Id(ophtalId, pacientId);
         if (exams.isEmpty()) {
-           //Return empty list if no exams are found
             return List.of();
         }
 
-        // Limit the range of exams to return
-        int safeEndIndex = Math.min(endIndex, exams.size());
-        List<ExamsResponse> responseList = exams.subList(startIndex, safeEndIndex).stream()
-                .map(e -> new ExamsResponse(e.getId(), e.getName(), e.getDate(), s3Service.generatePresignedUrl(e.getUrlImage())))
-                .collect(Collectors.toList());
-        return responseList;
+        return exams.stream()
+                    .skip(startIndex)
+                    .limit(Math.max(0, endIndex - startIndex))
+                    .map(this::mapToExamsResponse)
+                    .collect(Collectors.toList());
     }
 
     /**
-     * Deletes an exam based on the provided ophthalmologist ID, patient ID, and exam ID.
+     * Deletes an exam record from the database and removes the associated image from S3 storage.
      *
-     * @param ophtalIdString the ID of the ophthalmologist as a string
-     * @param pacientIdString the ID of the patient as a string
-     * @param examIdString the ID of the exam as a string
-     * @throws IllegalArgumentException if any of the provided IDs are invalid
-     * @throws EntityNotFoundException if the exam, patient, or ophthalmologist does not exist
+     * @param ophtalIdString the UUID string of the ophthalmologist
+     * @param pacientIdString the UUID string of the patient
+     * @param examIdString the UUID string of the exam
+     * @throws IllegalArgumentException if any of the UUID strings are invalid
+     * @throws EntityNotFoundException if the exam does not exist
      */
     @Transactional
     @Override
@@ -174,38 +126,78 @@ public class ExamServiceImpl implements ExamService {
         UUID pacientId = UUID.fromString(pacientIdString);
         UUID examId = UUID.fromString(examIdString);
 
-        // Verify the relaction between exam, pacient and ophtal are correct
-        verifyExam(ophtalId, pacientId, examId);
-        
-        s3Service.deleteImage(examRepository.findById(examId).get().getUrlImage());
-        System.out.println("Image deleted");
+        Exam exam = verifyExam(ophtalId, pacientId, examId);
+        s3Service.deleteImage(exam.getUrlImage());
 
-        // Delete exam
         examRepository.deleteById(examId);
+
     }
 
     /**
-     * Retrieves an exam by its ID, verifying the relationship between the exam, patient, and ophthalmologist.
+     * Retrieves an exam by its ID, along with the associated ophthalmologist and patient IDs.
      *
      * @param ophtalIdString the ID of the ophthalmologist as a string
      * @param pacientIdString the ID of the patient as a string
      * @param examIdString the ID of the exam as a string
      * @return an ExamRes object containing the details of the exam
-     * @throws IllegalArgumentException if the exam is not found or if the relationship verification fails
+     * @throws IllegalArgumentException if any of the provided IDs are invalid
      */
     @Override
     public ExamRes getExamById(String ophtalIdString, String pacientIdString, String examIdString) {
         UUID ophtalId = UUID.fromString(ophtalIdString);
         UUID pacientId = UUID.fromString(pacientIdString);
         UUID examId = UUID.fromString(examIdString);
-        // Verify the relaction between exam, pacient and ophtal are correct
-        verifyExam(ophtalId, pacientId, examId);
 
-        // find the exam
-        Exam exam = examRepository.findById(examId)
-                .orElseThrow(() -> new IllegalArgumentException("Exam not found"));
+        Exam exam = verifyExam(ophtalId, pacientId, examId);
 
-        // Create the exam on the Response format
+        return mapToExamRes(exam);
+    }
+
+    /**
+     * Finds a patient by their cedula and the doctor's ID.
+     *
+     * @param cedula the cedula (identification number) of the patient
+     * @param ophtalId the UUID of the doctor
+     * @return the patient found
+     * @throws IllegalArgumentException if no patient is found for the given cedula and doctor ID
+     */
+    private Pacient findPacientByCedulaAndDoctorId(String cedula, UUID ophtalId) {
+        return Optional.ofNullable(pacientRepository.findPacientByCedulaAndDoctorId(cedula, ophtalId))
+                       .orElseThrow(() -> new IllegalArgumentException("Patient not found for the given cedula"));
+    }
+
+    /**
+     * Creates an Exam object from the given ExamRequest and Pacient.
+     *
+     * @param examRequest the request object containing exam details
+     * @param pacient the Pacient object associated with the exam
+     * @return a new Exam object populated with data from the examRequest and pacient
+     */
+    private Exam createExamFromRequest(ExamRequest examRequest, Pacient pacient) {
+        return Exam.builder()
+                   .name(examRequest.name())
+                   .date(examRequest.date())
+                   .urlImage(examRequest.urlImage())
+                   .distanceRatio(examRequest.distanceRatio())
+                   .perimeterRatio(examRequest.perimeterRatio())
+                   .areaRatio(examRequest.areaRatio())
+                   .neuroretinalRimPerimeter(examRequest.neuroretinalRimPerimeter())
+                   .neuroretinalRimArea(examRequest.neuroretinalRimArea())
+                   .excavationPerimeter(examRequest.excavationPerimeter())
+                   .excavationArea(examRequest.excavationArea())
+                   .state(examRequest.state())
+                   .ddlStage(examRequest.ddlStage())
+                   .pacient(pacient)
+                   .build();
+    }
+
+    /**
+     * Maps an Exam entity to an ExamRes response object.
+     *
+     * @param exam the Exam entity to be mapped
+     * @return an ExamRes object containing the mapped data
+     */
+    private ExamRes mapToExamRes(Exam exam) {
         return new ExamRes(
                 exam.getId(),
                 exam.getName(),
@@ -220,37 +212,39 @@ public class ExamServiceImpl implements ExamService {
                 exam.getExcavationArea(),
                 exam.getState(),
                 exam.getDdlStage()
-                );
+        );
     }
 
     /**
-     * Verifies the existence of an exam and checks if the exam is associated with the given patient and 
-     * if the patient is associated with the given ophthalmologist.
+     * Maps an Exam object to an ExamsResponse object.
+     *
+     * @param exam the Exam object to be mapped
+     * @return an ExamsResponse object containing the mapped data from the Exam object
+     */
+    private ExamsResponse mapToExamsResponse(Exam exam) {
+        return new ExamsResponse(
+                exam.getId(),
+                exam.getName(),
+                exam.getDate(),
+                s3Service.generatePresignedUrl(exam.getUrlImage())
+        );
+    }
+
+    /**
+     * Verifies the existence and ownership of an exam by checking if the exam exists and if it belongs to the specified patient and ophthalmologist.
      *
      * @param ophtalId the UUID of the ophthalmologist
      * @param pacientId the UUID of the patient
-     * @param examId the UUID of the exam
-     * @throws IllegalArgumentException if the exam is not found
-     * @throws AccessDeniedException if the exam is not associated with the given patient or 
-     *         if the patient is not associated with the given ophthalmologist
+     * @param examId the UUID of the exam to be verified
+     * @return the verified Exam object if it exists and belongs to the specified patient and ophthalmologist
+     * @throws AccessDeniedException if the exam does not exist or does not belong to the specified patient and ophthalmologist
      */
-    public void verifyExam(UUID ophtalId, UUID pacientId, UUID examId) {
-        // Verify that the exam exist
-        Optional<Exam> examOptional = examRepository.findById(examId);
-        if (examOptional.isEmpty()) {
-            throw new IllegalArgumentException("Exam not found");
-        }
-        Exam exam = examOptional.get();
-
-        // Verify that the exam is associate to the pacient and the pacient is associate
-        // to the ophtal
-        Pacient pacient = exam.getPacient();
-
-        if (pacient == null || !pacient.getId().equals(pacientId)
-                || !pacient.getDoctorId().equals(ophtalId)) {
-            throw new AccessDeniedException("Unauthorized access to the exam");
-        }
-
+    private Exam verifyExam(UUID ophtalId, UUID pacientId, UUID examId) {
+        return examRepository.findById(examId)
+                             .filter(exam -> exam.getPacient() != null
+                                     && exam.getPacient().getId().equals(pacientId)
+                                     && exam.getPacient().getDoctorId().equals(ophtalId))
+                             .orElseThrow(() -> new AccessDeniedException("Unauthorized access to the exam"));
     }
 
 }
