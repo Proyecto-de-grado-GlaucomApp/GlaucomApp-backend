576ceb39c6ddf0364c448af6236dcfe403deaeee
ericksgv <sangaravi22@gmail.com>
2024-11-04 17:22:48 -0500

    Delete EmailNotFoundException.java

--- src/main/java/co/edu/javeriana/glaucomapp_backend/common/exceptions/EmailNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package co.edu.javeriana.glaucomapp_backend.common.exceptions;
-
-public class EmailNotFoundException extends RuntimeException {
-    public EmailNotFoundException(String message) {
-        super(message);
-    }
-}
